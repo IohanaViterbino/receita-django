d8c55a3fd0c8bbc844e716813abb1cd0545c6e85
Iohana <iohana.ma31@gmail.com>
2024-09-05 16:27:20 -0300

    feat: add initials models

--- receita/apps.py
+++ receita/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TarefaConfig(AppConfig):
+class ReceitaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'tarefa'
+    name = 'receita'
---
+++ receita/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    nome = models.CharField(max_length=255, null=False, blank=False)
+
+class Recipe(models.Model):
+    titulo = models.CharField(max_length=255, null=False, blank=False)
+    descricao = models.TextField(null=True, blank=True)
+    ingredientes = models.TextField(null=False, blank=False)
+    modo_preparo = models.TextField(null=False, blank=False)
+    tempo_preparo = models.IntegerField(null=False, blank=False)
+    publicado = models.BooleanField(default=False)
+    categoria = models.ForeignKey(to=Category, on_delete=models.SET_NULL, null=True, blank=False, related_name='receita')
+    autor = models.ForeignKey(to=User, on_delete=models.CASCADE, null=False, blank=False, related_name='receita')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Rating(models.Model):
+    comentario = models.TextField(null=True, blank=True)
+    usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='classificao')
+    receita = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='classificao')
+    nota = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 6)])
