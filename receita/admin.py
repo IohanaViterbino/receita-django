b8620264cd2e47e95b4c6eb8e6ac21b87267195d
Iohana <iohana.ma31@gmail.com>
2024-09-10 14:54:40 -0300

    fix: adjust view in admin

--- receita/admin.py
+++ receita/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from receita.models import Category, Rating, Recipe
 
-admin.site.register(Category)
-admin.site.register(Recipe)
-admin.site.register(Rating)
+# Corrigindo o nome das classes de admin para seguir a convenção de nomenclatura
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'publicado', 'autor', 'fk_categoria')
+    list_display_links = ('id', 'titulo')
+    search_fields = ('titulo', 'autor',)
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nota', 'fk_usuario', 'fk_receita')
+    list_display_links = ('id', 'nota')
+    search_fields = ('nota', 'fk_usuario', 'fk_usuario',)
+
+# Registrando os modelos com suas classes personalizadas no Django Admin
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Rating, RatingAdmin)
