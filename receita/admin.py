b98091d10cac1ba14ba4269984a8c413123ec703
Iohana <iohana.ma31@gmail.com>
2024-09-10 14:15:45 -0300

    fix: make migration and adding models for the view in admin

--- receita/admin.py
+++ receita/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from receita.models import Category, Rating, Recipe
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Recipe)
+admin.site.register(Rating)
--- receita/models.py
+++ receita/models.py
@@ -11,13 +11,13 @@ class Recipe(models.Model):
     modo_preparo = models.TextField(null=False, blank=False)
     tempo_preparo = models.IntegerField(null=False, blank=False)
     publicado = models.BooleanField(default=False)
-    fk_categoria = models.ForeignKey(to=Category, on_delete=models.SET_NULL, null=True, blank=False, related_name='receita')
-    autor = models.ForeignKey(to=User, on_delete=models.CASCADE, null=False, blank=False, related_name='receita')
+    fk_categoria = models.ForeignKey(to=Category, on_delete=models.SET_NULL, null=True, blank=False, related_name='recipe')
+    autor = models.ForeignKey(to=User, on_delete=models.CASCADE, null=False, blank=False, related_name='recipe')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Rating(models.Model):
     comentario = models.TextField(null=True, blank=True)
-    fk_usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='classificao')
-    fk_receita = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='classificao')
+    fk_usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='rating')
+    fk_receita = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='rating')
     nota = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
